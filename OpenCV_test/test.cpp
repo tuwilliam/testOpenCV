f6bed0373ea21d665d4e780d2917d35b6f5ec823
Chris Sun <ayszj@hotmail.com>
2017-04-19 21:10:00 +0800

    video area select

--- OpenCV_test/test.cpp
+++ OpenCV_test/test.cpp
@@ -1,52 +1,162 @@
-﻿/**
-@file videocapture_basic.cpp
-@brief A very basic sample for using VideoCapture and VideoWriter
-@author PkLab.net
-@date Aug 24, 2016
+﻿#include<opencv2\core\core.hpp>  
+#include<opencv2\highgui\highgui.hpp>  
+#include<opencv2\imgproc\imgproc.hpp>  
+using namespace cv;
+#include<iostream>  
+#include<vector>  
+using namespace std;
+/*----定义鼠标事件--画矩形区域：作用当两个车灯----*/
+//第一步：全局变量  
+bool drawing_box = false;
+bool gotBox = false;
+Rect box;
+Point downPoint;
+/*
+void mouseRectHandler(int event, int x, int y, int flags, void *param)
+{
+switch (event)
+{
+case CV_EVENT_MOUSEMOVE:
+if (drawing_box)
+{
+box.width = x - box.x;
+box.height = y - box.y;
+}
+break;
+case CV_EVENT_LBUTTONDOWN:
+drawing_box = true;
+box = Rect(x, y, 0, 0);
+break;
+case CV_EVENT_LBUTTONUP:
+drawing_box = false;
+gotBox = true;
+if (box.width < 0)
+{
+box.x += box.width;
+box.width *= -1;
+}
+if( box.height < 0 )
+{
+box.y += box.height;
+box.height *= -1;
+}
+break;
+default:
+break;
+}
+}
 */
 
-#include <opencv2/opencv.hpp>
-#include <iostream>
-#include <stdio.h>
-
-using namespace cv;
-using namespace std;
-
-int main(int, char**)
+void mouseRectHandler(int event, int x, int y, int flags, void *param)
 {
-	Mat frame;
-	//--- INITIALIZE VIDEOCAPTURE
-	VideoCapture cap;
-	// open the default camera using default API
-	cap.open(0);
-	// OR advance usage: select any API backend
-	int deviceID = 0;             // 0 = open default camera
-	int apiID = cv::CAP_ANY;      // 0 = autodetect default API
-								  // open selected camera using selected API
-	cap.open(deviceID + apiID);
-	// check if we succeeded
-	if (!cap.isOpened()) {
-		cerr << "ERROR! Unable to open camera\n";
-		return -1;
-	}
+	switch (event)
+	{
+	case CV_EVENT_MOUSEMOVE:
+		if (drawing_box)
+		{
+			//鼠标的移动到downPoint的右下角  
+			if (x >= downPoint.x && y >= downPoint.y)
+			{
+				box.x = downPoint.x;
+				box.y = downPoint.y;
+				box.width = x - downPoint.x;
+				box.height = y - downPoint.y;
+			}
+			//鼠标的移动到downPoint的右上角  
+			if (x >= downPoint.x && y <= downPoint.y)
+			{
+				box.x = downPoint.x;
+				box.y = y;
+				box.width = x - downPoint.x;
+				box.height = downPoint.y - y;
+			}
+			//鼠标的移动到downPoint的左上角  
+			if (x <= downPoint.x && y <= downPoint.y)
+			{
+				box.x = x;
+				box.y = y;
+				box.width = downPoint.x - x;
+				box.height = downPoint.y - y;
+			}
+			//鼠标的移动到downPoint的左下角  
+			if (x <= downPoint.x && y >= downPoint.y)
+			{
+				box.x = x;
+				box.y = downPoint.y;
+				box.width = downPoint.x - x;
+				box.height = y - downPoint.y;
+			}
+		}
+		break;
 
-	//--- GRAB AND WRITE LOOP
-	cout << "Start grabbing" << endl
-		<< "Press any key to terminate" << endl;
+	case CV_EVENT_LBUTTONDOWN:
+		//按下鼠标，代表可以可以开始画矩形  
+		drawing_box = true;
+		//记录起点  
+		downPoint = Point(x, y);
+		break;
+
+	case CV_EVENT_LBUTTONUP:
+
+		//松开鼠标，代表结束画矩形  
+		drawing_box = false;
+		gotBox = true;
+		break;
+	default:
+		break;
+	}
+}
+
+int main(int argc, char*argv[])
+{
+	//读取视频    
+	VideoCapture video(0);
+
+	//判断视频是否打开    
+	if (!video.isOpened())
+		return 0;
+
+	//视频帧    
+	Mat frame;
+
+
+	//register     
+	namedWindow("video", 1);
+	
+	setMouseCallback("video", mouseRectHandler, NULL);
+
+	//画感兴趣区域   
 	for (;;)
 	{
-		// wait for a new frame from camera and store it into 'frame'
-		cap.read(frame);
-		// check if we succeeded
-		if (frame.empty()) {
-			cerr << "ERROR! blank frame grabbed\n";
-			//break;
-		}
-		// show live and wait for a key with timeout long enough to show images
-		imshow("Live", frame);
-		if (waitKey(5) == 27)
+		//读取视频的第一帧    
+		video >> frame;
+		if (!frame.data)
+			break;
+		rectangle(frame, box, Scalar(255, 255, 0), 2);//画出感兴趣区域  
+		imshow("video", frame);
+		if (gotBox)
+			break;
+		if (waitKey(50) == 'q')//---------很重要    
 			break;
 	}
-	// the camera will be deinitialized automatically in VideoCapture destructor
+	//remove callback    
+	setMouseCallback("video", NULL, NULL);
+
+	namedWindow("video_select", 1);
+	//视频继续    
+	for (;;)
+	{
+		//读取视频   
+		video >> frame;
+		//判断是否有当前帧  
+		if (!frame.data)
+			break;
+		//画出感兴趣区域  
+		rectangle(frame, box, Scalar(0, 255, 0), 2);
+		imshow("video", frame);
+		imshow("video_select", frame(Rect(box.x, box.y, box.width, box.height)));
+		if (waitKey(33) == 'q')
+			break;
+	}
 	return 0;
 }
