571e09b722f8f067330ac674e34f034e2ac4e20a
tuwilliam <835203861@qq.com>
2017-04-20 14:35:21 +0800

    GaussianBlur

--- OpenCV_test/test.cpp
+++ OpenCV_test/test.cpp
@@ -117,40 +117,10 @@ int main(int argc, char*argv[])
 		return 0;
 
 	//视频帧    
-	Mat frame;
+	Mat frame,frameGaussian;
 
-
-	//register     
 	namedWindow("video", 1);
-	
-	setMouseCallback("video", mouseRectHandler, NULL);
-
-
-
-
-	//画感兴趣区域   
-	for (;;)
-	{
-		//读取视频的第一帧    
-		video >> frame;
-		if (!frame.data)
-			break;
-		rectangle(frame, box, Scalar(255, 255, 0), 2);//画出感兴趣区域  
-		imshow("video", frame);
-		if (gotBox)
-			break;
-		if (waitKey(50) == 'q')//---------很重要    
-			break;
-	}
-	//remove callback    
-	setMouseCallback("video", NULL, NULL);
-
-
-
-
-
-
-	namedWindow("video_select", 1);
+	namedWindow("video_Gaussian", 1);
 	//视频继续    
 	for (;;)
 	{
@@ -159,10 +129,11 @@ int main(int argc, char*argv[])
 		//判断是否有当前帧  
 		if (!frame.data)
 			break;
-		//画出感兴趣区域  
-		rectangle(frame, box, Scalar(0, 255, 0), 2);
+		GaussianBlur(frame, frameGaussian, Size(5, 5), 2, 1);
+
 		imshow("video", frame);
-		imshow("video_select", frame(Rect(box.x, box.y, box.width, box.height)));
+		imshow("video_Gaussian", frameGaussian);
+
 		if (waitKey(33) == 'q')
 			break;
 	}
