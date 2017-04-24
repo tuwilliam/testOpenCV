ff10c4652e58d090ce1f26ae8421b4db0789ed2a
tuwilliam <835203861@qq.com>
2017-04-24 21:37:06 +0800

    face detect ROI

--- OpenCV_test/test.cpp
+++ OpenCV_test/test.cpp
@@ -1,154 +1,234 @@
-﻿#include<opencv2\core\core.hpp>  
-#include<opencv2\highgui\highgui.hpp>  
-#include<opencv2\imgproc\imgproc.hpp>  
-using namespace cv;
-#include<iostream>  
-#include<vector>  
+﻿#include "opencv2/objdetect.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+
 using namespace std;
-/*----定义鼠标事件--画矩形区域：作用当两个车灯----*/
-//第一步：全局变量  
-bool drawing_box = false;
-bool gotBox = false;
-Rect box;
-Point downPoint;
-/*
-void mouseRectHandler(int event, int x, int y, int flags, void *param)
-{
-switch (event)
-{
-case CV_EVENT_MOUSEMOVE:
-if (drawing_box)
-{
-box.width = x - box.x;
-box.height = y - box.y;
-}
-break;
-case CV_EVENT_LBUTTONDOWN:
-drawing_box = true;
-box = Rect(x, y, 0, 0);
-break;
-case CV_EVENT_LBUTTONUP:
-drawing_box = false;
-gotBox = true;
-if (box.width < 0)
-{
-box.x += box.width;
-box.width *= -1;
-}
-if( box.height < 0 )
-{
-box.y += box.height;
-box.height *= -1;
-}
-break;
-default:
-break;
-}
-}
-*/
+using namespace cv;
 
-void mouseRectHandler(int event, int x, int y, int flags, void *param)
+static void help()
 {
-	switch (event)
+	cout << "\nThis program demonstrates the cascade recognizer. Now you can use Haar or LBP features.\n"
+		"This classifier can recognize many kinds of rigid objects, once the appropriate classifier is trained.\n"
+		"It's most known use is for faces.\n"
+		"Usage:\n"
+		"./facedetect [--cascade=<cascade_path> this is the primary trained classifier such as frontal face]\n"
+		"   [--nested-cascade[=nested_cascade_path this an optional secondary classifier such as eyes]]\n"
+		"   [--scale=<image scale greater or equal to 1, try 1.3 for example>]\n"
+		"   [--try-flip]\n"
+		"   [filename|camera_index]\n\n"
+		"see facedetect.cmd for one call:\n"
+		"./facedetect --cascade=\"../../data/haarcascades/haarcascade_frontalface_alt.xml\" --nested-cascade=\"../../data/haarcascades/haarcascade_eye_tree_eyeglasses.xml\" --scale=1.3\n\n"
+		"During execution:\n\tHit any key to quit.\n"
+		"\tUsing OpenCV version " << CV_VERSION << "\n" << endl;
+}
+
+void detectAndDraw(Mat& img, CascadeClassifier& cascade,
+	CascadeClassifier& nestedCascade,
+	double scale, bool tryflip);
+
+string cascadeName;
+string nestedCascadeName;
+Mat frame, image;
+vector<Rect> faces;
+Rect rectLast;
+bool detect = false;
+
+int main(int argc, const char** argv)
+{
+	VideoCapture capture;
+	
+	string inputName;
+	bool tryflip;
+	CascadeClassifier cascade, nestedCascade;
+	double scale;
+
+	cv::CommandLineParser parser(argc, argv,
+		"{help h||}"
+		"{cascade|haarcascade_frontalface_alt.xml|}"
+		"{nested-cascade|haarcascade_eye_tree_eyeglasses.xml|}"
+		"{scale|1|}{try-flip||}{@filename||}"
+		);
+	if (parser.has("help"))
 	{
-	case CV_EVENT_MOUSEMOVE:
-		if (drawing_box)
-		{
-			//鼠标的移动到downPoint的右下角  
-			if (x >= downPoint.x && y >= downPoint.y)
-			{
-				box.x = downPoint.x;
-				box.y = downPoint.y;
-				box.width = x - downPoint.x;
-				box.height = y - downPoint.y;
-			}
-			//鼠标的移动到downPoint的右上角  
-			if (x >= downPoint.x && y <= downPoint.y)
-			{
-				box.x = downPoint.x;
-				box.y = y;
-				box.width = x - downPoint.x;
-				box.height = downPoint.y - y;
-			}
-			//鼠标的移动到downPoint的左上角  
-			if (x <= downPoint.x && y <= downPoint.y)
-			{
-				box.x = x;
-				box.y = y;
-				box.width = downPoint.x - x;
-				box.height = downPoint.y - y;
-			}
-			//鼠标的移动到downPoint的左下角  
-			if (x <= downPoint.x && y >= downPoint.y)
-			{
-				box.x = x;
-				box.y = downPoint.y;
-				box.width = downPoint.x - x;
-				box.height = y - downPoint.y;
-			}
-		}
-		break;
-
-	case CV_EVENT_LBUTTONDOWN:
-		//按下鼠标，代表可以可以开始画矩形  
-		drawing_box = true;
-		//记录起点  
-		downPoint = Point(x, y);
-		break;
-
-	case CV_EVENT_LBUTTONUP:
-
-		//松开鼠标，代表结束画矩形  
-		drawing_box = false;
-		gotBox = true;
-		break;
-	default:
-		break;
-	}
-}
-
-Mat frame, GrayImage, frameCanny;
-int threshold_temp;
-
-void on_trackbar(int threshold)
-{
-	threshold_temp = threshold;
-	//canny边缘检测  
-	
-}
-
-int main(int argc, char*argv[])
-{
-	//读取视频    
-	VideoCapture video(0);
-	
-	//判断视频是否打开    
-	if (!video.isOpened())
+		help();
 		return 0;
-
-	namedWindow("video", 0);
-	namedWindow("videoCanny", 1);
-	int nThresholdEdge = 0;//初始值
-	cvCreateTrackbar("Threshold", "videoCanny", &nThresholdEdge, 255, on_trackbar);
-
-	//视频继续    
-	for (;;)
-	{
-		//读取视频   
-		video >> frame;
-		//判断是否有当前帧  
-		if (!frame.data)
-			break;
-		cvtColor(frame, GrayImage, CV_BGR2GRAY);
-
-		
-		
-		Canny(GrayImage, frameCanny, threshold_temp, threshold_temp * 3, 3);
-		on_trackbar(threshold_temp);
-		imshow("video", frame);
-		imshow("videoCanny", frameCanny);
-		if (waitKey(33) == 'q')
-			break;
 	}
+	cascadeName = parser.get<string>("cascade");
+	nestedCascadeName = parser.get<string>("nested-cascade");
+	scale = parser.get<double>("scale");
+	if (scale < 1)
+		scale = 1;
+	tryflip = parser.has("try-flip");
+	inputName = parser.get<string>("@filename");
+	if (!parser.check())
+	{
+		parser.printErrors();
+		return 0;
+	}
+	if (!nestedCascade.load(nestedCascadeName))
+		cerr << "WARNING: Could not load classifier cascade for nested objects" << endl;
+	if (!cascade.load(cascadeName))
+	{
+		cerr << "ERROR: Could not load classifier cascade" << endl;
+		help();
+		return -1;
+	}
+	if (inputName.empty() || (isdigit(inputName[0]) && inputName.size() == 1))
+	{
+		int camera = inputName.empty() ? 0 : inputName[0] - '0';
+		if (!capture.open(camera))
+			cout << "Capture from camera #" << camera << " didn't work" << endl;
+	}
+
+
+
+
+
+
+	if (capture.isOpened())
+	{
+		cout << "Video capturing has been started ..." << endl;
+
+		for (;;)
+		{
+			capture >> frame;
+			//frame = imread("lena.jpg");
+			if (frame.empty())
+				break;
+
+			
+			if (faces.size())
+			{
+				faces[0].x += rectLast.x;
+				faces[0].x -= 10;
+				if (faces[0].x < 0)
+					faces[0].x = 0;
+
+				if (faces[0].x > 400)
+					rectLast.x = faces[0].x;;
+				rectLast.x = faces[0].x;
+
+				faces[0].y += rectLast.y;
+				faces[0].y -= 10;
+				if (faces[0].y < 0)
+					faces[0].y = 0;
+				rectLast.y = faces[0].y;
+
+				faces[0].width += 20;
+				if (faces[0].width > frame.cols)
+					faces[0].width = frame.cols;
+				//rectLast.width = faces[0].width;
+
+				faces[0].height += 20;
+				if (faces[0].height > frame.rows)
+					faces[0].height = frame.rows;
+				//rectLast.height = faces[0].height;
+
+				
+
+				Mat frame1(frame, faces[0]);
+				detectAndDraw(frame1, cascade, nestedCascade, scale, tryflip);
+			}
+			else
+			{
+				rectLast.x = 0;
+				rectLast.y = 0;
+				Mat frame1 = frame.clone();
+				detectAndDraw(frame1, cascade, nestedCascade, scale, tryflip);
+			}
+
+			
+
+			char c = (char)waitKey(10);
+			if (c == 27 || c == 'q' || c == 'Q')
+				break;
+		}
+	}
+
 	return 0;
+}
+
+
+
+
+void detectAndDraw(Mat& img, CascadeClassifier& cascade,
+	CascadeClassifier& nestedCascade,
+	double scale, bool tryflip)
+{
+	double t = 0;
+	
+	const static Scalar colors[] =
+	{
+		Scalar(255,0,0),
+		Scalar(255,128,0),
+		Scalar(255,255,0),
+		Scalar(0,255,0),
+		Scalar(0,128,255),
+		Scalar(0,255,255),
+		Scalar(0,0,255),
+		Scalar(255,0,255)
+	};
+	Mat gray, smallImg;
+
+	
+		
+
+	cvtColor(img, gray, COLOR_BGR2GRAY);
+	double fx = 1 / scale;
+	resize(gray, smallImg, Size(), fx, fx, INTER_LINEAR);
+	equalizeHist(smallImg, smallImg);//增强对比度
+
+	t = (double)getTickCount();
+	cascade.detectMultiScale(smallImg, faces,
+		1.1, 2, 0
+		//|CASCADE_FIND_BIGGEST_OBJECT
+		//|CASCADE_DO_ROUGH_SEARCH
+		| CASCADE_SCALE_IMAGE,
+		Size(30, 30));
+	
+	t = (double)getTickCount() - t;
+	printf("detection time = %g ms\n", t * 1000 / getTickFrequency());
+
+	for (size_t i = 0; i < faces.size(); i++)
+	{
+		Rect r = faces[i];
+		Mat smallImgROI;
+		vector<Rect> nestedObjects;
+		Point center;
+		Scalar color = colors[i % 8];
+		int radius;
+
+		double aspect_ratio = (double)r.width / r.height;
+		if (0.75 < aspect_ratio && aspect_ratio < 1.3)
+		{
+			center.x = cvRound((r.x + r.width*0.5)*scale);
+			center.y = cvRound((r.y + r.height*0.5)*scale);
+			radius = cvRound((r.width + r.height)*0.25*scale);
+			circle(img, center, radius, color, 3, 8, 0);
+		}
+		else
+			rectangle(img, cvPoint(cvRound(r.x*scale), cvRound(r.y*scale)),
+				cvPoint(cvRound((r.x + r.width - 1)*scale), cvRound((r.y + r.height - 1)*scale)),
+				color, 3, 8, 0);
+		if (nestedCascade.empty())
+			continue;
+		smallImgROI = smallImg(r);
+		nestedCascade.detectMultiScale(smallImgROI, nestedObjects,
+			1.1, 2, 0
+			//|CASCADE_FIND_BIGGEST_OBJECT
+			//|CASCADE_DO_ROUGH_SEARCH
+			//|CASCADE_DO_CANNY_PRUNING
+			| CASCADE_SCALE_IMAGE,
+			Size(30, 30));
+		for (size_t j = 0; j < nestedObjects.size(); j++)
+		{
+			Rect nr = nestedObjects[j];
+			center.x = cvRound((r.x + nr.x + nr.width*0.5)*scale);
+			center.y = cvRound((r.y + nr.y + nr.height*0.5)*scale);
+			radius = cvRound((nr.width + nr.height)*0.25*scale);
+			circle(img, center, radius, color, 3, 8, 0);
+		}
+	}
+	imshow("result", frame);
 }
